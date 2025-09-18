ad6ea975124c15eef6f288848a3a21a265775c0f
Aaron <antvaaron@gmail.com>
2025-09-18 15:32:52 +0800

    refactor(jsx): optimize path to apply x/y attrs

--- packages/jsx/src/components/Path.ts
+++ packages/jsx/src/components/Path.ts
@@ -1,10 +1,18 @@
-import type { JSXElement, PathProps } from '../types';
+import type { GroupProps, JSXElement, PathProps } from '../types';
+import { Group } from './Group';
 
-export function Path(props: PathProps): JSXElement {
+export function Path({ x, y, width, height, ...props }: PathProps): JSXElement {
   const node: JSXElement = {
     type: 'path',
     props,
   };
+
+  const groupProps: GroupProps = {};
+  if (x !== undefined) groupProps.x = x;
+  if (y !== undefined) groupProps.y = y;
+  if (width !== undefined) groupProps.width = width;
+  if (height !== undefined) groupProps.height = height;
+  
   // TODO scale path to fit width/height
-  return node;
+  return Group({ ...groupProps, children: [node] });
 }
